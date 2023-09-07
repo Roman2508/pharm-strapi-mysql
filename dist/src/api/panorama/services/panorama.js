b0bab0eb64022facbb791daccc8deb0a30e407ee
Roman Ptashnyk <2508romaa@gmail.com>
2023-09-07 18:56:55 +0300

    commit

---
+++ dist/src/api/panorama/services/panorama.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * panorama service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::panorama.panorama');
